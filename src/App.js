9877b88ffbc5b0b6c60e89e21de04a601fa7fd9e
Benjamin Meena <59247187+BMeeNA57@users.noreply.github.com>
2020-07-27 16:04:48 -0600

    To Do- React Project
    First portion upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, {useState, useRef} from 'react';
+import TodoList from './TodoList'
+
+function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+  function handleAddTodo(e){
+      const name = todoNameRef.current.value
+      if (name ==='') return
+      setTodos(prevTodos => {
+        return [...prevTodos, {id: 1, name: name, complete:false}]
+      })
+      todoNameRef.current.value = null
+  }
+  return (
+    <>
+    <TodoList todos={todos} />
+    <input ref={todoNameRef}type="text" />
+    <button onClick={handleAddTodo}>Add Todo</button>
+    <button>Clear Todo's</button>
+    <div> 0 Left To do</div>
+    </>
+  )
+}
+
+export default App;
